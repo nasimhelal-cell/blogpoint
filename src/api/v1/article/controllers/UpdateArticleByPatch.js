9b6ca4e9a90d752d514ea13c5399b4616b51c2fc
Nasim <nasimhelal123>
2024-07-03 17:12:12 +0600

    Delete and patch and put request handling completed

--- src/api/v1/article/controllers/UpdateArticleByPatch.js
+++ src/api/v1/article/controllers/UpdateArticleByPatch.js
@@ -1,3 +1,38 @@
-function updateArticleByPatch(req, res) {}
+const articleService = require("../../../../lib/article");
+const { catchAsync, STATUS } = require("../../../../utils");
+
+const updateArticleByPatch = catchAsync(async (req, res) => {
+  const id = req.params.id;
+
+  const updates = req.body;
+
+  const article = await articleService.getArticle({ id });
+
+  if (article) {
+    const payload = Object.keys(updates).reduce((acc, key) => {
+      if (updates[key] !== undefined) {
+        acc[key] = updates[key];
+      }
+      return acc;
+    }, {});
+
+    Object.keys(payload).forEach((key) => {
+      article[key] = payload[key];
+    });
+    await article.save();
+  }
+
+  const response = {
+    code: STATUS.success.code,
+    message: "Article updated successfully",
+    data: article,
+    links: {
+      self: `/articles/${article.id}`,
+      author: `/articles/${article.id}/author`,
+      comments: `/articles/${article.id}/comments`,
+    },
+  };
+  res.status(STATUS.success.code).json(response);
+});
 
 module.exports = updateArticleByPatch;
--- src/api/v1/article/controllers/deleteSingleArticle.js
+++ src/api/v1/article/controllers/deleteSingleArticle.js
@@ -1,3 +1,24 @@
-function deleteSingleArticle(req, res) {}
+const { catchAsync, STATUS } = require("../../../../utils");
+const articleService = require("../../../../lib/article");
+const { AppError } = require("../../../../app/errorHandler");
+const deleteSingleArticle = catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const article = await articleService.getArticle({ id });
+
+  if (!article) {
+    throw new AppError(
+      "No article found with this id:" + id,
+      STATUS.notFound.code
+    );
+  } else {
+    await articleService.deleteArticle({ id });
+  }
+  const response = {
+    code: STATUS.success.code,
+    message: "Article deleted successfully",
+  };
+  res.status(STATUS.success.code).json(response);
+});
 
 module.exports = deleteSingleArticle;
